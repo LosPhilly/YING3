b5011fc430d0c77d8e68a4122508244604d8adc2
losphilly <cphillips8921@gmail.com>
2024-01-18 01:22:43 -0700

    Chat update

--- lib/Presentation/ChatScreens/IndividualChatScreens/widgets/chat_messages.dart
+++ lib/Presentation/ChatScreens/IndividualChatScreens/widgets/chat_messages.dart
@@ -9,81 +9,81 @@ class ChatMessages extends StatelessWidget {
   final messages = [
     Message(
         senderId: '2',
-        receiverId: 'gNfEHSQZ5ZUcY6JG5AarK8O0SVw1',
+        receiverId: 'fgKnNxiLQRc180qANUgi4tOn7AE2',
         content: 'Hello',
         sentTime: DateTime.now(),
         messageType: MessageType.text),
     Message(
-        senderId: 'gNfEHSQZ5ZUcY6JG5AarK8O0SVw1',
+        senderId: 'fgKnNxiLQRc180qANUgi4tOn7AE2',
         receiverId: '2',
         content: 'How are you?',
         sentTime: DateTime.now(),
         messageType: MessageType.text),
     Message(
         senderId: '2',
-        receiverId: 'gNfEHSQZ5ZUcY6JG5AarK8O0SVw1',
+        receiverId: 'fgKnNxiLQRc180qANUgi4tOn7AE2',
         content: 'Fine',
         sentTime: DateTime.now(),
         messageType: MessageType.text),
     Message(
-        senderId: 'gNfEHSQZ5ZUcY6JG5AarK8O0SVw1',
+        senderId: 'fgKnNxiLQRc180qANUgi4tOn7AE2',
         receiverId: '2',
         content: 'What are you doing?',
         sentTime: DateTime.now(),
         messageType: MessageType.text),
     Message(
         senderId: '2',
-        receiverId: 'gNfEHSQZ5ZUcY6JG5AarK8O0SVw1',
+        receiverId: 'fgKnNxiLQRc180qANUgi4tOn7AE2',
         content: 'Nothing',
         sentTime: DateTime.now(),
         messageType: MessageType.text),
     Message(
-        senderId: 'gNfEHSQZ5ZUcY6JG5AarK8O0SVw1',
+        senderId: 'fgKnNxiLQRc180qANUgi4tOn7AE2',
         receiverId: '2',
         content: 'Can you help me?',
         sentTime: DateTime.now(),
         messageType: MessageType.text),
     Message(
         senderId: '2',
-        receiverId: 'gNfEHSQZ5ZUcY6JG5AarK8O0SVw1',
+        receiverId: 'fgKnNxiLQRc180qANUgi4tOn7AE2',
         content:
             'https://images.unsplash.com/photo-1669992755631-3c46eccbeb7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
         sentTime: DateTime.now(),
         messageType: MessageType.image),
     Message(
-        senderId: 'gNfEHSQZ5ZUcY6JG5AarK8O0SVw1',
+        senderId: 'fgKnNxiLQRc180qANUgi4tOn7AE2',
         receiverId: '2',
         content: 'Thank you',
         sentTime: DateTime.now(),
         messageType: MessageType.text),
     Message(
       senderId: '2',
-      receiverId: 'gNfEHSQZ5ZUcY6JG5AarK8O0SVw1',
+      receiverId: 'fgKnNxiLQRc180qANUgi4tOn7AE2',
       content: 'You are welcome',
       sentTime: DateTime.now(),
       messageType: MessageType.text,
     ),
     Message(
-        senderId: 'gNfEHSQZ5ZUcY6JG5AarK8O0SVw1',
+        senderId: 'fgKnNxiLQRc180qANUgi4tOn7AE2',
         receiverId: '2',
         content: 'Bye',
         sentTime: DateTime.now(),
         messageType: MessageType.text),
     Message(
         senderId: '2',
-        receiverId: 'gNfEHSQZ5ZUcY6JG5AarK8O0SVw1',
+        receiverId: 'fgKnNxiLQRc180qANUgi4tOn7AE2',
         content: 'Bye',
         sentTime: DateTime.now(),
         messageType: MessageType.text),
     Message(
-        senderId: 'gNfEHSQZ5ZUcY6JG5AarK8O0SVw1',
+        senderId: 'fgKnNxiLQRc180qANUgi4tOn7AE2',
         receiverId: '2',
         content: 'See you later',
         sentTime: DateTime.now(),
         messageType: MessageType.text),
     Message(
         senderId: '2',
-        receiverId: 'gNfEHSQZ5ZUcY6JG5AarK8O0SVw1',
+        receiverId: 'fgKnNxiLQRc180qANUgi4tOn7AE2',
         content: 'See you later',
         sentTime: DateTime.now(),
         messageType: MessageType.text)
@@ -91,22 +91,25 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: messages.length,
-        itemBuilder: (context, index) {
-          final isTextMessage = messages[index].messageType == MessageType.text;
-          final isMe = receiverId != messages[index].senderId;
-          return isTextMessage
-              ? MessageBubble(
-                  isMe: isMe,
-                  message: messages[index],
-                  isImage: false,
-                )
-              : MessageBubble(
-                  isMe: isMe,
-                  message: messages[index],
-                  isImage: true,
-                );
-        });
+    return Expanded(
+      child: ListView.builder(
+          itemCount: messages.length,
+          itemBuilder: (context, index) {
+            final isTextMessage =
+                messages[index].messageType == MessageType.text;
+            final isMe = receiverId != messages[index].senderId;
+            return isTextMessage
+                ? MessageBubble(
+                    isMe: isMe,
+                    message: messages[index],
+                    isImage: false,
+                  )
+                : MessageBubble(
+                    isMe: isMe,
+                    message: messages[index],
+                    isImage: true,
+                  );
+          }),
+    );
   }
 }
