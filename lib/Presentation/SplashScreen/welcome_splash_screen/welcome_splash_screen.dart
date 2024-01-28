d438392ad9eaf3046058141d1ac335d1bc15f5da
losphilly <cphillips8921@gmail.com>
2024-01-28 13:58:17 -0700

    Splash Screen Transistions

--- lib/Presentation/SplashScreen/welcome_splash_screen/welcome_splash_screen.dart
+++ lib/Presentation/SplashScreen/welcome_splash_screen/welcome_splash_screen.dart
@@ -6,6 +6,8 @@ import 'package:ying_3_3/theme/custom_text_style.dart';
 import 'package:ying_3_3/theme/theme_helper.dart';
 import 'package:ying_3_3/user_state.dart';
 import 'package:ying_3_3/widgets/custom_image_view.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
 
 class WelcomeSplashScreen extends StatefulWidget {
   const WelcomeSplashScreen({Key? key})
@@ -23,59 +25,63 @@ class _WelcomeSplashScreenState extends State<WelcomeSplashScreen>
   void initState() {
     // TODO: implement initState
     super.initState();
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    /* SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
 
     Future.delayed(Duration(seconds: 4), () {
       Navigator.of(context).pushReplacement(
           MaterialPageRoute(builder: (_) => const UserState()));
-    });
+    }); */
   }
 
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-        overlays: SystemUiOverlay.values);
+    /* SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values); */
   }
 
   @override
   Widget build(BuildContext context) {
     mediaQueryData = MediaQuery.of(context);
 
-    return Scaffold(
-      backgroundColor: theme.colorScheme.primary,
-      body: SizedBox(
-        width: double.maxFinite,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 190.v,
-              width: 180.h,
-              child: Stack(
-                alignment: Alignment.bottomRight,
-                children: [
-                  CustomImageView(
-                      imagePath: ImageConstant.imgSplashYingLogo,
-                      height: 355.v,
-                      width: 374.h,
-                      alignment: Alignment.topCenter),
-                ],
+    return AnimatedSplashScreen(
+        duration: 5000,
+        splashIconSize: 255,
+        splash: SizedBox(
+          width: double.maxFinite,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 195.v,
+                width: 180.h,
+                child: Stack(
+                  alignment: Alignment.bottomRight,
+                  children: [
+                    CustomImageView(
+                        imagePath: ImageConstant.imgSplashYingLogo,
+                        height: 355.v,
+                        width: 374.h,
+                        alignment: Alignment.topCenter),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 15.v),
-            const Text(
-              "YING",
-              style: TextStyle(
-                  fontSize: 30.0, // Adjust the font size as needed
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
-            ),
-            SizedBox(height: 5.v),
-          ],
+              SizedBox(height: 15.v),
+              const Text(
+                "YING",
+                style: TextStyle(
+                    fontSize: 30.0, // Adjust the font size as needed
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              SizedBox(height: 5.v),
+            ],
+          ),
         ),
-      ),
-    );
+        nextScreen: const UserState(),
+        splashTransition: SplashTransition.fadeTransition,
+        //pageTransitionType: PageTransitionType.scale,
+        backgroundColor: theme.colorScheme.primary);
   }
 }
